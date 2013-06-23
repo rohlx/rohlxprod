550c02094baf9eefdb5a924d618c02a74de18c74
Muhilan MG <muhilanis@yahoo.co.in>
2013-06-23 10:21:07 -0500

    added marker for the google maps

--- rohlxwebapp/src/main/webapp/js/rohlxcustom.js
+++ rohlxwebapp/src/main/webapp/js/rohlxcustom.js
@@ -1,14 +1,35 @@
+
+
+
+function addMarker(map,latlong,title,content)
+{
+  var markerOptions = {
+    position: latlong,
+    map:map,
+    title : title,
+    clickable : true
+  };
+  var marker = new google.maps.Marker(markerOptions);
+}
+
 function initialize() {
+  var googleLatandLogRohlx = new google.maps.LatLng(8.148,77.568455);	
   var mapOptions = {
     zoom: 18,
-    center: new google.maps.LatLng(8.148,77.568455),
+    center: googleLatandLogRohlx,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+  
+  
   /** Needed to bring the map into the view **/
-  $('#map-canvas')[0].scrollIntoView()
+  $('#map-canvas')[0].scrollIntoView();
+  
+  addMarker(map,googleLatandLogRohlx,"Rohlx","Rohlx1"); 
+  
+ 
 }
 
 function loadScript() {
@@ -18,7 +39,7 @@ function loadScript() {
   {
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&' +
       'callback=initialize';
   document.body.appendChild(script);
   }
