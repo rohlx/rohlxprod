e897a2546bd14557dce3a1bc5d27abfc798e5df8
Muhilan MG <muhilanis@yahoo.co.in>
2013-06-28 19:00:33 -0500

    modularised the js

--- rohlxwebapp/src/main/webapp/js/rohlxcustom.js
+++ rohlxwebapp/src/main/webapp/js/rohlxcustom.js
@@ -1,72 +1,77 @@
 /** Custom javascript for rohlx **/
+var rohlx = rohlx || {};
 
+(function($, mod, undefined) {
 
-/** Function to add the marker for rohlx **/
-function addMarker(map,latlong,title,content)
-{
-  var markerOptions = {
-    position: latlong,
-    map:map,
-    title : title,
-    clickable : true
-  };
-  var marker = new google.maps.Marker(markerOptions);
-}
+	mod.init = function() {
+		$("#openmaps").unbind().click(mod._loadScript);
+		$(window).load(mod._animate);
+	};
 
-/** Loads and Initializes sript when the address link is clicked **/
-function initialize() {
-  var googleLatandLogRohlx = new google.maps.LatLng(8.148,77.568455);	
-  var mapOptions = {
-    zoom: 18,
-    center: googleLatandLogRohlx,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+	/** Function to add the marker for rohlx **/
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-  
-  
-  /** Needed to bring the map into the view **/
-  $('#map-canvas')[0].scrollIntoView();
-  
-  addMarker(map,googleLatandLogRohlx,"Rohlx","Rohlx1"); 
- 
-}
+	mod._addMarker = function(map, latlong, title, content) {
+		var markerOptions = {
+			position : latlong,
+			map : map,
+			title : title,
+			clickable : true
+		};
+		var marker = new google.maps.Marker(markerOptions);
+	};
 
-/** Call to load google maps script **/
-function loadScript() {
-    
-  if(!isGoogleAPILoadedAlready())
-  {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&' +
-      'callback=initialize';
-  document.body.appendChild(script);
-  }
-  
-} 
+	/** Loads and Initializes sript when the address link is clicked **/
+	mod.initialize = function() {
+		var googleLatandLogRohlx = new google.maps.LatLng(8.148, 77.568455);
+		var mapOptions = {
+			zoom : 18,
+			center : googleLatandLogRohlx,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
 
-/** Script to add animation to load the sections for the menus **/
-function animate(){
-$('.scroller').click(function(){
-var section = $($(this).data("section"));
-var top = section.offset().top-82;
-$("html, body").animate({ scrollTop: top }, 700);
-return false;
-});
-}
+		var map = new google.maps.Map(document.getElementById('map-canvas'),
+				mapOptions);
 
-/** This function checks if the javascript to load the google api is already loaded or not **/
-function isGoogleAPILoadedAlready(){
-    if($('script[src*="maps.googleapis.com"]').val() === undefined)
-    {
-        return false;
-    }
-    return true;
-}
+		/** Needed to bring the map into the view **/
+		$('#map-canvas')[0].scrollIntoView();
 
+		mod._addMarker(map, googleLatandLogRohlx, "Rohlx", "Rohlx1");
 
-/** Other things to be loaded **/
-$("#openmaps").unbind().click(loadScript);
-$(window).load(animate);
+	};
+
+	/** Call to load google maps script **/
+	mod._loadScript = function() {
+		if (!mod._isGoogleAPILoadedAlready()) {
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&'
+					+ 'callback=rohlx.initialize';
+			document.body.appendChild(script);
+		}
+
+	};
+
+	/** Script to add animation to load the sections for the menus **/
+	mod._animate = function() {
+		$('.scroller').click(function() {
+			var section = $($(this).data("section"));
+			var top = section.offset().top - 82;
+			$("html, body").animate({
+				scrollTop : top
+			}, 700);
+			return false;
+		});
+	};
+
+	/** This function checks if the javascript to load the google api is already loaded or not **/
+	mod._isGoogleAPILoadedAlready = function() {
+		if ($('script[src*="maps.googleapis.com"]').val() === undefined) {
+			return false;
+		}
+		return true;
+	};
+
+})($, rohlx);
+
+/** invoke first method **/
+$(rohlx.init);
