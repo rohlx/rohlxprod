ea99ec77a92e0372c27e203df86882d94ea2aadd
Muhilan MG <muhilanis@yahoo.co.in>
2013-07-09 22:42:43 -0500

    random number generation logic

--- rohlxwebapp/src/main/java/com/rohlx/util/email/EmailHelperUtil.java
+++ rohlxwebapp/src/main/java/com/rohlx/util/email/EmailHelperUtil.java
@@ -1,23 +1,35 @@
 package com.rohlx.util.email;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Map;
 
 public class EmailHelperUtil {
-	
-	public static String buildBody(Map<String, String[]> values)
-	{
+
+	public static String buildBody(Map<String, String[]> values) {
 		String email = values.get("email")[0];
 		String phone = values.get("phone")[0];
-		String name =  values.get("name")[0];
+		String name = values.get("name")[0];
 		String message = values.get("message")[0];
-		
-		return "Hi, Please find the message from customer "+ name +"\n" + message + "\n" + "Phone Number : "+ phone + "\n"+ " Email  : "+email;
-	}
-	
-	public static String getGeneratedRequestNumber()
-	{
-		return new Integer(Calendar.getInstance().MILLISECOND).toString();
+
+		return "Hi, Please find the message from customer " + name + "\n"
+				+ message + "\n" + "Phone Number : " + phone + "\n"
+				+ " Email  : " + email;
 	}
 
+	public static String getGeneratedRequestNumber() {
+		StringBuilder sb = new StringBuilder();
+		GregorianCalendar gc = (GregorianCalendar) Calendar.getInstance();
+		sb.append(prependZeros(gc.get(Calendar.MONTH) + 1))
+				.append(prependZeros(gc.get(Calendar.DATE)))
+				.append(gc.get(Calendar.HOUR)).append(gc.get(Calendar.MINUTE))
+				.append(gc.get(Calendar.SECOND)).append((int)Math.floor(Math.random()*10));
+		System.out.println(sb.toString());
+		return sb.toString();
+	}
+
+	private static String prependZeros(int i) {
+		String s = new Integer(i).toString();
+		return s.length() == 1 ? "0" + s : s;
+	}
 }
---
+++ rohlxwebapp/src/test/java/com/rohlx/test/TestUtil.java
@@ -0,0 +1,16 @@
+package com.rohlx.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rohlx.util.email.EmailHelperUtil;
+
+public class TestUtil {
+	@Test
+	public void test() {
+		assertFalse(EmailHelperUtil.getGeneratedRequestNumber().equals(
+				EmailHelperUtil.getGeneratedRequestNumber()));
+	}
+}
